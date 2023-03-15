436d9239c2511f407bf9cf8996812f50a7db400a
IonutGrosu <ionutflorentin.grosu@gmail.com>
2023-03-15 16:08:28 +0100

    Added test endpoint for getting a 3D model as json

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-const express = require('express')
+import { NodeIO, Document } from '@gltf-transform/core';
+import fs from "fs/promises";
+import express from 'express';
+
 const app = express()
 const port = 3000
 
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/3dmodel', async (req, res) => {
+    const io = new NodeIO();
+
+    var gltfmodel = new Document();
+    
+    gltfmodel = await io.read("./3dmodel/mario.gltf");
+    
+    gltfmodel.getRoot().listTextures()[0].setImage(await fs.readFile('./greenTexture.png'));
+    gltfmodel.getRoot().listTextures()[0].setURI("greenTexture.png");
+    
+    var jsonModel = await io.writeJSON(gltfmodel).then((json)=> {
+        return json;
+    })
+    
+    res.send(jsonModel);
 })
 
 
