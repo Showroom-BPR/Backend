dc4407aed4906150677bf2c8ec2fcbcb8e56f82b
IonutGrosu <ionutflorentin.grosu@gmail.com>
2023-04-19 15:53:48 +0200

    Implemented the use of tmp folder for each request

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import { get3DAsset } from "./services/3dAssetsService.js";
+import { v4 as uuidv4 } from "uuid";
+import * as fs from "fs";
+import * as os from "os";
 
 dotenv.config();
 
@@ -12,12 +15,36 @@ app.get("/", (_, res) => {
 });
 
 app.get("/3DAsset", async (req, res) => {
+  const processId: string = uuidv4();
+  console.log(processId);
+
   const username = req.query.username.toString();
   const productId = req.query.productId.toString();
 
-  const asset3D = await get3DAsset(username, productId);
+  const processTempFolderName: string = `${os.tmpdir()}/LEGO_SHOWROOM/${processId}`;
 
-  res.send(asset3D);
+  try {
+    fs.mkdirSync(processTempFolderName, {});
+    const asset3D = await get3DAsset(
+      username,
+      productId,
+      processTempFolderName
+    );
+    res.send(asset3D);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  } finally {
+    try {
+      if (processTempFolderName) {
+        fs.rmSync(processTempFolderName, { recursive: true });
+      }
+    } catch (e) {
+      console.error(
+        `An error has occurred while removing the temp folder at ${processTempFolderName}. Please remove it manually. Error: ${e}`
+      );
+    }
+  }
 });
 
 app.listen(port, () => {
--- src/services/3dAssetsService.ts
+++ src/services/3dAssetsService.ts
@@ -5,15 +5,22 @@ import { SaveBufferToFile } from "../fsUtils.js";
 
 const textures_bucket = "virtual-showroom-textures";
 const models_bucket = "virtual-showroom-3dmodels";
+const textureFileType = ".png";
 
 export async function get3DAsset(
   username: string,
-  productId: string
+  productId: string,
+  processTempFolderName: string
 ): Promise<Uint8Array> {
-  const result_texture_name = `${username}-${productId}.png`;
+  const result_texture_name = `${username}-${productId}${textureFileType}`;
+
   const asset3dName = `${productId}.glb`;
 
-  await downloadS3Object(models_bucket, asset3dName, asset3dName);
+  await downloadS3Object(
+    models_bucket,
+    asset3dName,
+    `${processTempFolderName}/${asset3dName}`
+  );
 
   if (await existsInS3(textures_bucket, result_texture_name)) {
     console.log("Watermarked texture exists.");
@@ -21,15 +28,21 @@ export async function get3DAsset(
     await downloadS3Object(
       textures_bucket,
       result_texture_name,
-      result_texture_name
+      `${processTempFolderName}/${result_texture_name}`
     );
   } else {
     console.log("Watermarked texture does not exist. Will be created now");
 
     const result = await createWatermarkedTexture(username, productId);
     const buffer = Buffer.from(result, "binary");
-    await SaveBufferToFile(buffer, result_texture_name);
+    await SaveBufferToFile(
+      buffer,
+      `/${processTempFolderName}/${result_texture_name}`
+    );
   }
 
-  return await replaceTexture(result_texture_name, asset3dName);
+  return await replaceTexture(
+    `${processTempFolderName}/${result_texture_name}`,
+    `${processTempFolderName}/${asset3dName}`
+  );
 }
